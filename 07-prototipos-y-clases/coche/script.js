0b0a79d42263a36e2b4cb8bc68145f87e7dfabcc
Álvaro Barrena <alvarobarrena.dev@gmail.com>
2025-11-18 19:26:27 +0100

    Completed car exercise

---
+++ 07-prototipos-y-clases/coche/script.js
@@ -0,0 +1,70 @@
+console.log("Ejercicio coche");
+
+class Coche {
+  #encendido;
+  #velocidad;
+  #marca;
+  #modelo;
+  #matricula;
+
+  constructor(marca, modelo, matricula) {
+    this.#marca = marca;
+    this.#modelo = modelo;
+    this.#matricula = matricula;
+    this.#encendido = false;
+    this.#velocidad = 0;
+  }
+
+  arrancar() {
+    this.#encendido = true;
+    console.log("Coche arrancado.");
+  }
+  
+  apagar() {
+    if (this.#velocidad !== 0) {
+      this.#encendido = false;
+    } else {
+      console.info(`El vehículo va a ${this.#velocidad} km/h, pon la velocidad a 0 km/h`);
+    }
+  }
+  
+  acelerar() {
+    if (this.#encendido) {
+      this.#velocidad += 10;
+      console.log(`Velocidad actual ${this.#velocidad} km/h.`);
+    } else {
+      console.info("El coche no está encendido");
+    }
+  }
+  
+  desacelerar() {
+    if (this.#encendido) {
+      if (this.#velocidad > 0) {
+        this.#velocidad -= 10;
+        console.log(`Velocidad actual ${this.#velocidad} km/h.`);
+        if (this.#velocidad < 0) {
+          this.#velocidad = 0;
+        }
+      } else {
+        console.info("El coche no puede desacelerar porque no está andando.");
+      }
+    } else {
+      console.log("El coche no puede acelerar porque no está arrancado");
+    }
+  }
+
+  toString() {
+    console.log(`${this.#marca} ${this.#modelo} - Matrícula ${this.#matricula}`);
+  }
+}
+
+const coche = new Coche("BMW", "M4", "1234ALV");
+console.log(coche.toString());
+
+coche.arrancar()
+coche.acelerar();
+coche.apagar();
+coche.desacelerar();
+console.log(coche);
+coche.desacelerar();
+console.log(coche);
