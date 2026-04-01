a99c62f02c81417597f03cd7fb2eaceaedc7c6df
Álvaro Barrena <alvarobarrena.dev@gmail.com>
2026-04-01 11:56:40 +0200

    Remove all files related to the "21-ejercicio-de-parejas" project, including source code, styles, and configuration files.

--- 21-ejercicio-de-parejas/src/main.js
+++
@@ -1,9 +0,0 @@
-import './sass/main.scss';
-import Game from './class/Game';
-
-let resetButton = document.getElementById('reset');
-resetButton.addEventListener('click', () => Game.resetGame());
-
-Game.getRowsCols().then((data) => {
-  let game = new Game(data.rows, data.cols, "game");
-});
